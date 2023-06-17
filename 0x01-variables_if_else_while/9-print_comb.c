1d21900448882de29d840ce7f4fa801811e3993d
sallyMohamed <engsally@gmail.com>
2023-06-16 17:42:02 -0700

    last mandatory

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main-program entry point.
+ *
+ * Return:0 if no errors, non zero if error
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < '9'; num++)
+	{
+		putchar(num);
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
