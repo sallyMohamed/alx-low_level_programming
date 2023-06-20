f6894844e28342cf6b337e36c7c62b4e6cf61fe5
sallyMohamed <engsally@gmail.com>
2023-06-20 15:45:08 -0700

    print sgin

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign -check sign.
+ *
+ * description: character to check .
+ *
+ * @n:character to check .
+ *
+ *Return: 0-if zero,1-if + and -1 if -.
+ *
+ */
+int print_sign(int n)
+{
+	int n;
+
+	for (n = 0; n <= n; n++)
+	{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == '0')
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	}
+}
